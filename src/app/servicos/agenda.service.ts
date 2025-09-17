af236d4c990e5451b97c08851b7fde1ce572854b
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-09-17 12:00:21 -0300

    Desenvolvimento da agendaService no anuglar

---
+++ src/app/servicos/agenda.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Agenda } from '../classes/agenda';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AgendaService {
+
+  private apiUrl = 'https://localhost:7165/api/Agenda';
+
+  constructor(private http: HttpClient) { }
+
+  cadastrar(agenda: Agenda): Observable<Agenda> {
+    return this.http.post<Agenda>(this.apiUrl, agenda);
+  }
+
+  adicionarAgenda(agenda: Agenda): Observable<Agenda> {
+    return this.http.post<Agenda>(`${this.apiUrl}/AdicionarAgenda`, agenda);
+  }
+
+  listarAgenda(): Observable<Agenda[]> {
+    return this.http.get<Agenda[]>(`${this.apiUrl}/ListarAgenda`);
+  }
+
+  listarAgendasAtivas(): Observable<Agenda[]> {
+    return this.http.get<Agenda[]>(`${this.apiUrl}/ListarAgendasAtivas`);
+  }
+
+  listarAgendasInativas(): Observable<Agenda[]> {
+    return this.http.get<Agenda[]>(`${this.apiUrl}/ListarAgendasInativas`);
+  }
+
+  listarAgendasPendentes(): Observable<Agenda[]> {
+    return this.http.get<Agenda[]>(`${this.apiUrl}/ListarAgendasPendentes`);
+  }
+
+}
